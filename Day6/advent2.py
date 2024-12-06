f308099fc005846733b0008b427eba25697f5f38
Matt-J-Dong <matthew.dong@nyu.edu>
2024-12-06 01:15:11 -0500

    Day6

---
+++ Day6/advent2.py
@@ -0,0 +1,55 @@
+with open('./text3.txt', 'r') as f:
+    lines = f.readlines()
+n = len(lines)
+
+total = 1
+flag = False
+blocks = []
+for output in range(n):
+    lines[output] = lines[output].strip()
+    line = lines[output]
+    print(line)
+    if(line.find("^")>=0):
+        current = [output,line.find("^")]
+    location = 0
+    flag2 = True
+    flag3 = True
+    while flag2 == True:
+        if(flag3 == True): #HOWWWW
+            loc = line.find("#")
+        else:
+            loc = line.find("#",location+1)
+        if(loc>-1):
+            #print(f"{loc}")
+            blocks.insert(len(blocks),[output,loc])
+            location = loc
+            flag3 = False
+        else:
+            flag2 = False
+guard = True
+guard_direction = [[-1,0],[0,1],[1,0],[0,-1]]
+num = 0
+guard_location = current.copy()
+print(blocks)
+visits = [guard_location.copy()]
+direction_visits = [[guard_location.copy(),guard_direction.copy()]]
+while guard == True:
+    #print(guard_direction)
+    #print(guard_location)
+    guard_location[0] = guard_location[0]+guard_direction[num][0]
+    guard_location[1] = guard_location[1]+guard_direction[num][1]
+    if(guard_location in blocks):
+        guard_location[0] = guard_location[0]-guard_direction[num][0]
+        guard_location[1] = guard_location[1]-guard_direction[num][1]
+        num=(num+1)%4
+        #print(num)
+    #print(guard_location)
+    if(guard_location not in direction_visits):
+        direction_visits.append([guard_location.copy(),guard_direction.copy()])
+        #print("yoy")
+    if(guard_location[0]>(n-1) or guard_location[0]<0 or guard_location[1]>(len(line)-1) or guard_location[1]<0):
+        guard = False
+for output in range(n):
+    lines[output] = lines[output].strip()
+    line = lines[output]
+    
